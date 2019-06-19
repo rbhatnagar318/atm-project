9f90fc6414125468dc65c94b4779bc1cb86aa1b9
Varsha Kumari <varsha.k@imimobile.com>
2019-06-19 12:34:55 +0530

    removed loop and empty the amount after click

--- src/app/components/atm-money-dispenser/atm-money-dispenser.component.ts
+++ src/app/components/atm-money-dispenser/atm-money-dispenser.component.ts
@@ -8,14 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class AtmMoneyDispenserComponent implements OnInit {
   amount: any
   totalNotes: any
-  dispensedNotes={}
+  dispensedNotes = {}
   constructor() { }
 
   ngOnInit() {
     this.initFunc();
   }
 
-  initFunc(){
+  initFunc() {
 
   }
 
@@ -29,15 +29,15 @@ export class AtmMoneyDispenserComponent implements OnInit {
     var dispensedNotesArray = Object.keys(this.dispensedNotes).map((item) => {
       return parseInt(item);
     }).reverse();
-    for (var i = 0; i < dispensedNotesArray.length; i++) {
-      for (var j = 0; j < dispensedNotesArray.length; j++) {
-        if (this.amount >= dispensedNotesArray[j]) {
-          var value = this.amount / dispensedNotesArray[j];
-          this.dispensedNotes[dispensedNotesArray[j]] = parseInt(value.toString().split('.')[0]);
-          this.amount = this.amount % dispensedNotesArray[j];
-        }
+
+    for (var j = 0; j < dispensedNotesArray.length; j++) {
+      if (this.amount >= dispensedNotesArray[j]) {
+        var value = this.amount / dispensedNotesArray[j];
+        this.dispensedNotes[dispensedNotesArray[j]] = parseInt(value.toString().split('.')[0]);
+        this.amount = this.amount % dispensedNotesArray[j];
       }
     };
-    this.totalNotes = Object.values(this.dispensedNotes).reduce((a:number, b:number) => a + b);
+    this.amount = '';
+    this.totalNotes = Object.values(this.dispensedNotes).reduce((a: number, b: number) => a + b);
   }
 }
